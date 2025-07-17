10d1ce38d203096d647beff15b50b05532f5395a
Wadim293 <136265508+Wadim293@users.noreply.github.com>
2025-07-17 18:33:32 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "7979288273:AAHepAqmcgaKXOm-QHW_jev2adqqSU2G8yw"
 WEBHOOK_PATH = "webhook"
-WEBHOOK_URL = f"https://parser-production-52bb.up.railway.app//{WEBHOOK_PATH}"
+WEBHOOK_URL = f"https://parser-production-52bb.up.railway.app/{WEBHOOK_PATH}"
 WEBHOOK_PORT = 8080
 
 API_ID = 2040
