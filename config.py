480aa30e675c8c8dbd760080ee7d2c8471cf161e
Wadim293 <136265508+Wadim293@users.noreply.github.com>
2025-07-17 18:30:05 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = "7979288273:AAHepAqmcgaKXOm-QHW_jev2adqqSU2G8yw"
 WEBHOOK_PATH = "webhook"
-WEBHOOK_URL = f"https://da29f6e7a6e1.ngrok-free.app/{WEBHOOK_PATH}"
-WEBHOOK_PORT = 3000
+WEBHOOK_URL = f"https://parser-production-52bb.up.railway.app//{WEBHOOK_PATH}"
+WEBHOOK_PORT = 8080
 
 API_ID = 2040
 API_HASH = "b18441a1ff607e10a989891a5462e627"
